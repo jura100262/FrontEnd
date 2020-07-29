0f4ddf277b6ed387d27e19dd544907b09b00e27a
Jura <omega100262@gmail.com>
2020-07-29 16:48:34 +0300

    .

---
+++ JavaScript/JS/Introduction/script.js
@@ -0,0 +1,23 @@
+function demo1() {
+    document.getElementById('demo1').innerHTML = 'Hello , JavaScript!';
+}
+
+function demo2() {
+    document.getElementById('myImage').src = 'images/pic_bulbon.gif';
+}
+
+function demo3() {
+    document.getElementById('myImage').src = 'images/pic_bulboff.gif';
+}
+
+function demo4() {
+    document.getElementById('demo4').style.fontSize = '35px';
+}
+
+function demo5() {
+    document.getElementById('demo5').style.display = 'none';
+}
+
+function demo6() {
+    document.getElementById('demo6').style.display = 'block';
+}
